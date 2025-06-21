3769946eecf5844d2bfd33f2173df8921442f7f8
iThorgrim <contact@warlog.dev>
2025-06-21 19:08:01 +0200

    fix(Player): increase PLAYER_EXPLORED_ZONES_SIZE from 128 to 255

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -69,7 +69,7 @@ typedef void(*bgZoneRef)(Battleground*, WorldPackets::WorldState::InitWorldState
 
 #define PLAYER_MAX_SKILLS           127
 #define PLAYER_MAX_DAILY_QUESTS     25
-#define PLAYER_EXPLORED_ZONES_SIZE  128
+#define PLAYER_EXPLORED_ZONES_SIZE  255
 
 // corpse reclaim times
 #define DEATH_EXPIRE_STEP (5*MINUTE)
