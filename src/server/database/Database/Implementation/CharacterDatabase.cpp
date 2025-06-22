1075a575e25482e6725ce6e1e7aea28b248d92c9
iThorgrim <contact@warlog.dev>
2025-06-22 21:38:47 +0200

    fix(CharacterDatabase): change connection type for character_pseudo_item queries to synchronous

--- src/server/database/Database/Implementation/CharacterDatabase.cpp
+++ src/server/database/Database/Implementation/CharacterDatabase.cpp
@@ -622,8 +622,7 @@ void CharacterDatabaseConnection::DoPrepareStatements()
         "`stat_type1`, `stat_value1`, `stat_type2`, `stat_value2`, `stat_type3`, `stat_value3`, "
         "`stat_type4`, `stat_value4`, `stat_type5`, `stat_value5`, `stat_type6`, `stat_value6`, "
         "`stat_type7`, `stat_value7`, `stat_type8`, `stat_value8`, `stat_type9`, `stat_value9`, "
-        "`stat_type10`, `stat_value10` "
-        "FROM `character_pseudo_item`", CONNECTION_ASYNC);
+        "`stat_type10`, `stat_value10` FROM `character_pseudo_item`", CONNECTION_SYNCH);
     PrepareStatement(CHAR_SEL_PSEUDO_ITEM_DATA, "SELECT * FROM `character_pseudo_item` WHERE `guid` = ?", CONNECTION_SYNCH);
     PrepareStatement(CHAR_INS_PSEUDO_ITEM_DATA, "INSERT INTO `character_pseudo_item` ("
         "`guid`, `name`, `level`, "
